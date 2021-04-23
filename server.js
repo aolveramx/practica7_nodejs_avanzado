6284ca939fceb7c4b8b49ba9dc9de62268e86763
Ari Olvera <badillo.ari@gmail.com>
2021-04-23 15:19:34 -0500

    feat(DataBase): connection, unhandled rejector & colors

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-
-//Route files
-const articles = require('./routes/articles')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 //Load env variables
 dotenv.config({ path: './config/config.env' })
+
+//Connect to database
+connectDB()
+
+//Route files
+const articles = require('./routes/articles')
 
 //Init express
 const app = express()
@@ -23,5 +28,12 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `.yellow.bold)
 )
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  //Close server & exit process
+  server.close(() => process.exit(1))
+})
