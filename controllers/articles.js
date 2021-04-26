17f9068765e5c48742ca9f128f9f2d10b9d9cf68
Ari Olvera <badillo.ari@gmail.com>
2021-04-25 19:05:41 -0500

    refactor(async/await): middleware to avoid try catch

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 const Article = require('../models/Article')
 
 /**
@@ -6,107 +7,86 @@ const Article = require('../models/Article')
  * @route GET /api/v1/articles
  * @access Public
  */
-exports.getArticles = async (req, res, next) => {
-  try {
-    const articles = await Article.find()
+exports.getArticles = asyncHandler(async (req, res, next) => {
+  const articles = await Article.find()
 
-    res
-      .status(200)
-      .json({
-      success: true,
-      count: articles.length,
-      data: articles
-    })
-  } catch (err) {
-    res.status(400).json({ success: false })
-  }
-}
+  res
+    .status(200)
+    .json({
+    success: true,
+    count: articles.length,
+    data: articles
+  })
+})
 
 /**
  * @desc Get single article
  * @route GET /api/v1/articles/:id
  * @access Public
  */
-exports.getArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findById(req.params.id)
+exports.getArticle = asyncHandler(async (req, res, next) => {
+  const article = await Article.findById(req.params.id)
 
-    if(!article) {
-      return next(
-        new ErrorResponse(`No encontramos el artículo con if ${req.params.id}`, 404)
-      )
-    }
-
-    res.status(200).json({ 
-      success: true, 
-      data: article 
-    })
-
-  } catch (err) {
-    next(
+  if(!article) {
+    return next(
       new ErrorResponse(`No encontramos el artículo con if ${req.params.id}`, 404)
     )
   }
-}
+
+  res.status(200).json({ 
+    success: true, 
+    data: article 
+  })
+})
 
 /**
  * @desc Create new article
  * @route POST /api/v1/articles
  * @access Private
  */
-exports.createArticle = async (req, res, next) => {
-  try {
-    const article = await Article.create(req.body)
+exports.createArticle = asyncHandler(async (req, res, next) => {
+  const article = await Article.create(req.body)
 
-    res.status(201).json({ 
-      success: true, 
-      data: article 
-    })
-
-  } catch (err) {
-    return res.status(400).json({ success: false })
-  }
-}
+  res.status(201).json({ 
+    success: true, 
+    data: article 
+  })
+})
 
 /**
  * @desc Update article
  * @route PUT /api/v1/article/:id
  * @access Private
  */
-exports.updateArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    })
+exports.updateArticle = asyncHandler(async (req, res, next) => {
+  const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
 
-    if(!article) {
-      return res.status(400).json({ success: false })
-    }
-
-    res.status(200).json({ success: true, data: article })
-    
-  } catch (err) {
-    return res.status(400).json({ success: false })
+  if(!article) {
+    return next(
+      new ErrorResponse(`No encontramos el artículo con if ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({ success: true, data: article })
+  
+})
 
 /**
  * @desc Delete article
  * @route DELETE /api/v1/articles/:id
  * @access Private
  */
-exports.deleteArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findByIdAndDelete(req.params.id)
+exports.deleteArticle = asyncHandler(async (req, res, next) => {
+  const article = await Article.findByIdAndDelete(req.params.id)
 
-    if(!article) {
-      return res.status(400).json({ success: false })
-    }
-
-    res.status(200).json({ success: true, data: {} })
-    
-  } catch (err) {
-    return res.status(400).json({ success: false })
+  if(!article) {
+    return next(
+      new ErrorResponse(`No encontramos el artículo con if ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({ success: true, data: {} })
+})
