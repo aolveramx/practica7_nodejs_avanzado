af64d85f01d3fb675a03321ef41767c53bfda3fd
Ari Olvera <badillo.ari@gmail.com>
2021-04-29 12:18:52 -0500

    feat(middleware): auth protect

--- routes/articles.js
+++ routes/articles.js
@@ -9,20 +9,22 @@ const {
 } = require('../controllers/articles')
 const router = express.Router()
 
+const { protect } = require('../middleware/auth')
+
 const Articles = require('../models/Article')
 const advancedResults = require('../middleware/advancedResults')
 
-router.route('/:id/photo').put(articlePhotoUpload)
+router.route('/:id/photo').put(protect, articlePhotoUpload)
 
 router
   .route('/')
   .get(advancedResults(Articles), getArticles)
-  .post(createArticle)
+  .post(protect, createArticle)
 
 router
   .route('/:id')
   .get(getArticle)
-  .put(updateArticle)
-  .delete(deleteArticle)
+  .put(protect, updateArticle)
+  .delete(protect, deleteArticle)
 
 module.exports = router
