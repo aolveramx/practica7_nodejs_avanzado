4900ae267a0125b14ccb32949539000e9049020a
Ari Olvera <badillo.ari@gmail.com>
2021-04-30 16:28:56 -0500

    feat(forgotPassword): send email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const sendEmail = require('../utils/sendEmail')
 const User = require('../models/User')
 
 /**
@@ -80,6 +81,32 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken()
 
   await user.save({ validateBeforeSave: false })
+
+  //Create reset URL
+  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`
+
+  const message = `Estas recibiendo este correo electrónico porque solicitaste un reinicio de tu contraseña. Realiza un PUT request a: \n\n ${resetURL}`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Reinicio de contraseña',
+      message
+    })
+
+    res.status(200).json({
+      success: true,
+      data: 'Email sent'
+    })
+  } catch (err) {
+    console.log(err)
+    user.getResetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save({ validateBeforeSave: false })
+
+    return next(new ErrorResponse('No se pudo enviar el correo electrónico', 500))
+  }
 
   res.status(200).json({
     success: true,
