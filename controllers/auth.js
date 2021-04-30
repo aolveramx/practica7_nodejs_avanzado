05ec8ac18cf2514d691c1cd85c850df2b4ffea26
Ari Olvera <badillo.ari@gmail.com>
2021-04-30 16:42:31 -0500

    feat: resetPassword

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 const sendEmail = require('../utils/sendEmail')
@@ -112,6 +113,41 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     success: true,
     data: user
   })
+})
+
+/**
+ * @desc   Reset password
+ * @route  PUT /api/v1/auth/resetpassword/:resettoken
+ * @access Public
+ */
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  //Get hashed token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resettoken)
+    .digest('hex')
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() }
+  })
+
+  if (!user) {
+    return next(new ErrorResponse('Token no válido', 400))
+  }
+
+  //Set new password
+  user.password = req.body.password
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpire = undefined
+  await user.save()
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+
+  sendTokenResponse(user, 200, res)
 })
 
 //Get token from model, create cookie and send response
